22ec54ad5ae7617aef2dc043f4ca0d8580262911
Bianca Amarante Magalhães <164433450+byancaaam@users.noreply.github.com>
2024-06-11 17:57:40 -0300

    refactor: atualizando o script para a index receber os componentes header e o footer

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-  fetch('pages/homePage/homePage.html')
+  fetch('components/header.html')
     .then((response) => response.text())
     .then((data) => {
-      document.getElementById('home-container').innerHTML = data;
+      document.getElementById('header').innerHTML = data;
+    })
+    .catch((error) => {
+      console.error(
+        'There has been a problem with your fetch operation:',
+        error
+      );
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('components/footer.html')
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById('footer').innerHTML = data;
     })
     .catch((error) => {
       console.error(
