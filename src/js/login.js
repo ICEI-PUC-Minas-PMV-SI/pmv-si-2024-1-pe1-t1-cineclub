9aefd3400c9c7eb4ceb54c18d525318cfbb0c11c
Bianca Amarante Magalhães <164433450+byancaaam@users.noreply.github.com>
2024-06-11 21:05:25 -0300

    refactor: unindo a tela de login a tela inicial

---
+++ src/js/login.js
@@ -0,0 +1,14 @@
+//Carregando o footer
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('../components/footer.html')
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById('footer').innerHTML = data;
+    })
+    .catch((error) => {
+      console.error(
+        'There has been a problem with your fetch operation:',
+        error
+      );
+    });
+});
