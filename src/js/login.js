9dec95a2e2d2dbc5e324ffeaaf56957c45cdb78c
Bianca Amarante Magalhães <164433450+byancaaam@users.noreply.github.com>
2024-06-16 01:08:39 -0300

    fix: apagando conteudo de teste do arquivos login.js

--- src/js/login.js
+++ src/js/login.js
@@ -1,14 +1,1 @@
-//Carregando o footer
-document.addEventListener('DOMContentLoaded', function () {
-  fetch('../components/footer.html')
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById('footer').innerHTML = data;
-    })
-    .catch((error) => {
-      console.error(
-        'There has been a problem with your fetch operation:',
-        error
-      );
-    });
-});
+
