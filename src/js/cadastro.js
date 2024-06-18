e87c453d778fa8b0b99c580bda2b95688eb253bb
Bianca Amarante Magalhães <164433450+byancaaam@users.noreply.github.com>
2024-06-18 01:53:17 -0300

    fix: deletando modo de carregamento antigo da pagina cadastro

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -1,13 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('pages/cadastro.html')
-      .then((response) => response.text())
-      .then((data) => {
-        document.getElementById('cadastro-container').innerHTML = data;
-      })
-      .catch((error) => {
-        console.error(
-          'There has been a problem with your fetch operation:',
-          error
-        );
-      });
-  });
+
